5e4aac47cb7b34bda40a30c56935229d87f91d6f
mohamed <mohamedabdelwahidi@gmail.com>
2023-08-19 17:57:31 +0100

    des

--- back/index.js
+++ back/index.js
@@ -5,6 +5,7 @@ import appointRoute from './routes/appointment.js';
 import contactRoute from './routes/contact.js';
 import bodyParser from  'body-parser';
 import  cors from  'cors';
+import path from 'path'
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)
@@ -23,11 +24,18 @@ app.use(cors());
 
 app.use("/api/optician", appointRoute)
 app.use("/api/users", contactRoute)
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/front/dist')));
+app.get('*', (req, res) => 
+res.sendFile(path.join(__dirname, '/front/dist/index.html'))
+)
 
 app.use((err, req, res, next) => {
   res.status(500).send({message: err.message});
 })
-const port = process.env.PORT || 5000; 
+
+const port = process.env.PORT || 4000; 
 app.listen(port, () => {
     console.log(`server at http://localhost:${port}`);
   });
