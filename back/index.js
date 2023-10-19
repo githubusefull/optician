1303161b518c632a2e05828b62295944ab271901
mohamed <mohamedabdelwahidi@gmail.com>
2023-10-19 16:51:53 +0100

    change origin in index

--- back/index.js
+++ back/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(
     cors({
       credentials: true,
-      origin: ['http://127.0.0.1:5173'],
+      origin: ['https://optician-doctor.web.app'],
     })
   )
 app.use(express.json());
